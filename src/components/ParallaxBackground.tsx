9757a2f958070da855a178c9d8e9a85f2d1e6906
Seu Nome <seu-email@exemplo.com>
2025-09-06 15:09:34 -0300

    remove: DarkVeil component - remove import e uso do DarkVeil do ParallaxBackground, deleta arquivo DarkVeil.tsx, mantém apenas CircularText no background

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { motion } from 'motion/react';
 import CircularText from './CircularText';
-import DarkVeil from './DarkVeil';
 
 const ParallaxBackground = () => {
   const [scrollY, setScrollY] = useState(0);
@@ -14,19 +13,6 @@ const ParallaxBackground = () => {
 
   return (
     <div className="fixed inset-0 pointer-events-none">
-      {/* DarkVeil background effect with site colors */}
-      <div className="absolute inset-0 opacity-40">
-        <DarkVeil
-          hueShift={0} // Blue hue to match primary color #648fe0
-          noiseIntensity={0.05}
-          scanlineIntensity={0.2}
-          speed={0.5}
-          scanlineFrequency={3}
-          warpAmount={0.1}
-          resolutionScale={1}
-        />
-      </div>
-      
       {/* Single large circular text fixed in position */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <CircularText
